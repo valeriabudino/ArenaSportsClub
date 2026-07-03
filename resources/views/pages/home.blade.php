5d021153d4914b6fcd30d7b309ae99e6e3b66e82
Matias Nuñez <yo.matiasdn19@gmail.com>
2026-07-03 15:27:53 -0300

    feat: crear layout publico y home inicial

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.public')
+
+@section('content')
+    <h1>ArenaSportsClub</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::view('/', 'pages.home')->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
