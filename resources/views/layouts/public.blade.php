da614c390c8780e02e4471bede2f0bbcc51573d6
Matias Nuñez <yo.matiasdn19@gmail.com>
2026-07-06 12:13:00 -0300

    favicon

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ArenaSportsClub</title>
-
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-[#f5f5f5] text-[#101814] font-sans antialiased">
